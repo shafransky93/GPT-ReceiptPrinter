ae316e3d49a8ec83aa0265ea67a83382ffead3d7
Benjamin Shafransky <shafransky93@gmail.com>
2023-09-22 18:29:05 -0700

    Update crisis.bat

--- crisis.bat
+++ crisis.bat
@@ -5,5 +5,5 @@ start "Check Brain" check_brain.bat
 start "You Pass the Butter" python you_pass_the_butter.py
 timeout /t 100
 taskkill /f /im python.exe
-python receipt_print.py
+python brain_print.py
 goto loop
